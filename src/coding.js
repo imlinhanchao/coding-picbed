04061bd28221d9633b7248bb141b1b0ae4e2a52a
hancel.lin <imlinhanchao@foxmail.com>
2020-03-05 17:43:00 +0800

    完成获取部署 Page 服务地址

--- src/coding.js
+++ src/coding.js
@@ -19,6 +19,36 @@ module.exports = function ({
             token,
         });
 
-        return rsp.shared
+        return rsp.data.depot.shared
+    }
+
+    async function getPageUrl() {
+        let sites = await request({
+            api: `https://${user}.coding.net/api/user/${user}/project/${project}/autodeploy/static-sites`,
+            token,
+        });
+        let data = {};
+        if (!sites.data) return data;
+        sites = sites.data;
+        for (let i = 0; i < sites.list.length; i++) {
+            let l = sites.list[i];
+            let id = l.id;
+            let pages = await request({
+                api: `https://${user}.coding.net/api/user/${user}/project/${project}/autodeploy/static-sites/${id}`,
+                token,
+            })
+            let deploys = await request({
+                api: `https://${user}.coding.net/api/user/${user}/project/${project}/autodeploy/static-sites/${id}/tasks?page=1&page_size=10`,
+                token,
+            });
+            let lastDeploy = deploys.data.list[deploys.data.list.length - 1];
+            data[lastDeploy.depot_name] = pages.data.available_addresses
+        }
+        return data;
+    }
+
+    return {
+        isShare,
+        getPageUrl
     }
 }
